701aa92e5a0ef6dc274667543f327dfd223c0e6a
nandatritona <nandhafebrian3@gmail.com>
2023-04-18 10:02:45 +0700

    openAI backend

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { generateimage } = require('../controllers/openaiController');
+const router = express.Router();
+
+router.post('/generateimage', generateimage);
+
+module.exports = router;
